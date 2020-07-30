20dfab3c37627541ea9a834b8347c855060ab673
winvenchang <2120676359@qq.com>
2020-07-30 17:26:21 +0800

    分类、归档和标签页

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,8 @@ import markdown
 import re
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
-from django.shortcuts import render, get_object_or_404 ,HttpResponse
-from .models import Post
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from .models import Post, Category, Tag
 
 
 # Create your views here.
@@ -26,3 +26,25 @@ def detail(request, pk):
     m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
     post.toc = m.group(1) if m is not None else ''
     return render(request, 'blog/detail.html', context={'post': post})
+
+
+def archive(request, year, month):
+    """归档页面"""
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    """分类页面"""
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def tag(request, pk):
+    """标签页面"""
+    t = get_object_or_404(Tag, pk=pk)
+    post_list = Post.objects.filter(tags=t).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
