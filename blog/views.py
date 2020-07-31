5145d0a77b3f3b7ac455bc91c27a4e02af8b0285
winvenchang <2120676359@qq.com>
2020-07-31 14:59:30 +0800

    Django官方推荐的姿势：类视图

--- blog/views.py
+++ blog/views.py
@@ -5,50 +5,58 @@ from markdown.extensions.toc import TocExtension
 from django.shortcuts import render, get_object_or_404, HttpResponse
 from .models import Post, Category, Tag
 
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
-def index(request):
-    post_list = Post.objects.all().order_by('-created_time')
-    return render(request, 'blog/index.html', context={
-        'post_list': post_list
-    })
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    paginate_by = 10
 
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+class PostDetailview(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
 
-    # 阅读量 +1
-    post.increase_views()
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
 
-    md = markdown.Markdown(extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        TocExtension(slugify=slugify),
-    ])
-    post.body = md.convert(post.body)
+    def get_object(self, querset=None):
+        post = super().get_object(queryset=None)
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
 
-    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
-    post.toc = m.group(1) if m is not None else ''
-    return render(request, 'blog/detail.html', context={'post': post})
+        m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+        post.toc = m.group(1) if m is not None else ''
+        return post
 
 
-def archive(request, year, month):
+class ArchiveView(IndexView):
     """归档页面"""
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month
-                                    ).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super().get_queryset().filter(created_time__year=year, created_time__month=month)
 
 
-def category(request, pk):
+class CategoryView(IndexView):
     """分类页面"""
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super().get_queryset().filter(category=cate)
 
 
-def tag(request, pk):
+class TagView(IndexView):
     """标签页面"""
-    t = get_object_or_404(Tag, pk=pk)
-    post_list = Post.objects.filter(tags=t).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+    def get_queryset(self):
+        cate = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super().get_queryset().filter(tags=cate)
